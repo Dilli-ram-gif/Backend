bdb2c6152175eabe7d52ea4d2f98b3ae8270c287
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-02-23 08:29:35 +0545

    working of middleware

--- Improved_workflow/app.js
+++ Improved_workflow/app.js
@@ -3,15 +3,25 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next(); //allows the next to continue next request to the middleware in line
+app.use('/', (req, res, next) =>{
+  console.log("This always runs!");
+  next();
 })
 
-app.use((req, res, next) => {
-  console.log("In the another middleware!");
-  res.write('<h1>Hello from Express!</h1>')
+app.use('/add-product', (req, res, next) => {
+  console.log("In another middleware!");
+  res.send('<h1>The add product page!</h1>'); 
 })
+
+app.use('/', (req, res, next) => {
+  console.log("In another middleware!");
+  res.send('<h1> Hello from Express!</h1>'); 
+})
+
+// app.use((req, res, next) => {
+//   console.log("In the another middleware!");
+//   res.write('<h1>Hello from Express!</h1>')
+// })
 
 
 app.listen(3000);
