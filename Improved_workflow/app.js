24c1a19d4acf64a8f26a3a8ee34b01a933f45aa2
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-02-23 08:04:07 +0545

    middleware function used and port used in short form

--- Improved_workflow/app.js
+++ Improved_workflow/app.js
@@ -1,5 +1,19 @@
-const http = require("http");
-const routes = require('./routes')
-console.log(routes.someText)
-const server = http.createServer(routes.handler);
-server.listen(3000);
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next(); //allows the next to continue next request to the middleware in line
+})
+
+app.use((req, res, next) => {
+  console.log("In the another middleware!");
+  res.write('<h1>Hello from Express!</h1>')
+})
+
+
+app.listen(3000);
+// const server = http.createServer(app);
+// server.listen(3000);
