02bf8418f2fd0693da1330ffa6014747efe50678
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-02-25 08:38:39 +0545

    parsing incoming requests

--- Improved_workflow/app.js
+++ Improved_workflow/app.js
@@ -1,28 +1,37 @@
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/', (req, res, next) =>{
-  console.log("This always runs!");
-  next();
-})
 
-app.use('/add-product', (req, res, next) => {
+// app.use("/", (req, res, next) => {
+//   console.log("This always runs!");
+//   next();
+// });
+
+app.use("/add-product", (req, res, next) => {
   console.log("In another middleware!");
-  res.send('<h1>The add product page!</h1>'); 
-})
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>'
+  );
+});
 
-app.use('/', (req, res, next) => {
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   console.log("In another middleware!");
-  res.send('<h1> Hello from Express!</h1>'); 
-})
+  res.send("<h1> Hello from Express!</h1>");
+});
 
 // app.use((req, res, next) => {
 //   console.log("In the another middleware!");
 //   res.write('<h1>Hello from Express!</h1>')
 // })
-
 
 app.listen(3000);
 // const server = http.createServer(app);
